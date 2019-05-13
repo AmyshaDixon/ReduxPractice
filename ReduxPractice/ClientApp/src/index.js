26373e057c0944c99d32225be42b9368a4b33bf0
Amy Dixon <amysha.dixon@gmail.com>
2019-05-12 18:10:23 -0700

    Began store creation in index.js

--- ReduxPractice/ClientApp/src/index.js
+++ ReduxPractice/ClientApp/src/index.js
@@ -4,9 +4,29 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'; // Must be camel-case or cannot be found in redux
+
+// Seeting this up for the store instead of
+// holding it within Counter.js (moving state up);
+// Will be used with reducer which *can* use an initial/existing 
+// state to begin
+const initialState = {
+    count: 0
+};
+
+// Creating reducer to be used with createStore;
+// used to return existing/new state
+function reducer(state = initialState, action) {
+    console.log('reducer', state, action);
+
+    return state;
+}
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+
+// Create store from redux
+const store = createStore(reducer);
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
